8f37a44b39a7783536d1d6efd0e72ab869f7f6c7
visualStats <48520438+visualStats@users.noreply.github.com>
2023-12-22 12:38:32 +0000

    Updates

--- script.js
+++ script.js
@@ -4,11 +4,9 @@ $(document).ready(function () {
         "url": "https://cdn.jsdelivr.net/gh/visualStats/small-area-stats/urban_areas.json",
         "dataType": "json",
         "success": function (result) {
-            debugger
             $("select[name=select-area]").empty().append($("<option>")).select2({
                 theme: 'bootstrap-5',
                 minimumInputLength: 0,
-                //  width: '100%',
                 placeholder: "Select a Town/Urban Area",
                 data: result
             }).on('select2:select', function (e) {
