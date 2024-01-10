de51ebd32f6090e5250b7f5714f3076311f78ce7
visualStats <48520438+visualStats@users.noreply.github.com>
2024-01-10 11:01:12 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
     $.ajax({
         "url": "https://cdn.jsdelivr.net/gh/visualStats/small-area-stats/urban_areas.json",
         "dataType": "json",
-        "success": function (result) {
+        "success": function(result) {
             renderThemes();
             $("select[name=select-area]").empty().append($("<option>")).select2({
                 theme: 'bootstrap-5',
                 minimumInputLength: 0,
                 placeholder: "Select a Town/Urban Area",
                 data: result
-            }).on('select2:select', function (e) {
+            }).on('select2:select', function(e) {
                 if ($(this).val()) {
+                    $("#indicators-wrapper").empty();
                     fetchSmallAreas(true);
                 }
             });
         }
     });
 
-    $("select[name=theme]").on("change", renderData);
+    $("select[name=theme]").on("change", function() {
+        $("#indicators-wrapper").empty();
+        renderData();
+    });
 
 });
