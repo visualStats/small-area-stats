a1679d16686208ae0f2e077b3f4092df576d455f
visualStats <48520438+visualStats@users.noreply.github.com>
2024-01-12 09:38:18 +0000

    Fix scroll

--- js/script.js
+++ js/script.js
@@ -26,7 +26,7 @@ $(document).ready(function () {
     });
 
     $(window).scroll(function () {
-        if ($(this).scrollTop() > 50) {
+        if ($(this).scrollTop() > 600) {
             $('#back-to-top').fadeIn();
         } else {
             $('#back-to-top').fadeOut();
